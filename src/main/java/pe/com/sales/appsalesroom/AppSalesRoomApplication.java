29ac7c8c0a251a9069169bcb9884da709b946a8c
abraham peralta <abraham20467k@gmail.com>
2024-05-18 18:04:50 -0500

    second commit

---
+++ src/main/java/pe/com/sales/appsalesroom/AppSalesRoomApplication.java
@@ -0,0 +1,13 @@
+package pe.com.sales.appsalesroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSalesRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSalesRoomApplication.class, args);
+    }
+
+}
